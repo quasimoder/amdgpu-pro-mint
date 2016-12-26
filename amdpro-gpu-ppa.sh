56698d8e98a0c9dfe7790cd96d10d06e406546cd
quasimoder <quasimoder@hotmail.de>
2016-12-26 11:50:40 +0100

    changes

--- amdpro-gpu-ppa.sh
+++ amdpro-gpu-ppa.sh
@@ -1,7 +1,13 @@
 cd /tmp
 mkdir amdgpu-pro-ppa
 cd amdgpu-pro-ppa
+wget https://raw.githubusercontent.com/quasimoder/amdgpu-pro-mint/master/amd_mint.patch
 wget --referer=http://support.amd.com/en-us/kb-articles/Pages/AMDGPU-PRO-Driver-for-Linux-Release-Notes.aspx https://www2.ati.com/drivers/linux/ubuntu/amdgpu-pro-16.50-362463.tar.xz
 tar -Jxvf amdgpu-pro-16.50-362463.tar.xz
 cd amdgpu-pro-16.50-362463
+patch /tmp/amdgpu-pro-ppa/amdgpu-pro-16.50-362463/amdgpu-pro-install /tmp/amdgpu-pro/amd_mint.patch
+apt update
+apt upgrade -y
+apt install cdbs dh-make dkms execstack dh-modaliases linux-headers-generic libqtgui4 xserver-xorg-dev debhelper lib32gcc1 -y
+sh ./tmp/amdgpu-pro-ppa/amdgpu-pro-16.50-362463/amdgpu-pro-install -y
 
