3c75d37f9672d9d0d027823acc16bd6f2ea39def
quasimoder <quasimoder@hotmail.de>
2016-12-26 11:42:02 +0100

    init

---
+++ amdpro-gpu-ppa.sh
@@ -0,0 +1,7 @@
+cd /tmp
+mkdir amdgpu-pro-ppa
+cd amdgpu-pro-ppa
+wget --referer=http://support.amd.com/en-us/kb-articles/Pages/AMDGPU-PRO-Driver-for-Linux-Release-Notes.aspx https://www2.ati.com/drivers/linux/ubuntu/amdgpu-pro-16.50-362463.tar.xz
+tar -Jxvf amdgpu-pro-16.50-362463.tar.xz
+cd amdgpu-pro-16.50-362463
+
